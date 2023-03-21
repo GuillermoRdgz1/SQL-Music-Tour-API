a87e002c0f21959fec89f8dfc670937b96184aa7
guillermo rodriguez <guillermordgz.96@gmail.com>
2023-03-20 20:41:42 -0700

    migrations

---
+++ migrations/20230321031309-create-band.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('band', {
+      band_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      available_start_time: {
+        type: Sequelize.DATE
+      },
+      end_time: {
+        type: Sequelize.DATE
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('band');
+  }
+};
